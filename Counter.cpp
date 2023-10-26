39eeec994ee575b67b04c5223d2f8355dba38c49
Vitalya <vitalyabes.vb@gmail.com>
2023-10-26 11:00:24 +0300

    Third commit

--- Counter.cpp
+++ Counter.cpp
@@ -5,8 +5,6 @@
 class Counter {
 private:
 
-    std::string YesorNot;
-    char command;
     int num;
 
 public:
@@ -18,49 +16,68 @@ public:
         this->num = num;
     }
 
-   
-    void StartProgram() {
-        std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-        std::cin >> YesorNot;
-        while (YesorNot != "да" && YesorNot != "нет") {
-                std::cout << "Вы ввели неправильное значение \n";
-                std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-                std::cin >> YesorNot;
-          } 
-        if (YesorNot == "да") {
-            std::cout << "Введите начальное значение счётчика: ";
-            std::cin >> num;
-           
-        }
-        
+    void add() {
+        num++;
     }
-    
-    void Calculation() {
-        while (command != 'x' && command != 'х') {
-            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-            std::cin >> command;
+    void subtraction() {
+        num--;
+    }
+    void equals() {
+        std::cout << num <<"\n";
+    }
 
-            switch (static_cast<char>(command)) {
-            case '+': num++; break;
-            case '-': num--; break;
-            case '=': std::cout << num <<"\n"; break;
-            case 'x': std::cout << "До свидания!"; break;
-            case 'х': std::cout << "До свидания!"; break;
-            default: std::cout <<"Ошибка!Такого знака нет или не предусмотрен программой\n"; break;
-            }
-        }
-    }
+  
 };
+
+
+void Command(Counter & counter) {
+    char command;
+    do {
+        
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+
+        switch (static_cast<char>(command)) {
+        case '+': counter.add(); break;
+        case '-': counter.subtraction(); break;
+        case '=': counter.equals(); break;
+        case 'x': std::cout << "До свидания!"; break;
+        case 'х': std::cout << "До свидания!"; break;
+        default: std::cout << "Ошибка!Такого знака нет или не предусмотрен программой\n"; break;
+        }
+    } while (command != 'x' && command != 'х');
+}
+
+
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
+
+    std::string YesorNot;
+    int num;
     Counter counter;
-    counter.StartProgram();
-    counter.Calculation();
- 
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> YesorNot;
+    while (YesorNot != "да" && YesorNot != "нет") {
+        std::cout << "Вы ввели неправильное значение \n";
+        std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+        std::cin >> YesorNot;
+    }
+    if (YesorNot == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> num;
+        Counter counter(num);
+
+        Command(counter);
+    }
+   
+    else {
+        Command(counter);
+
+    }
+   
     return 0;
 }
 
